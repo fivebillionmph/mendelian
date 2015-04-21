998a763bd79954b44f6e50f403885c3f681d11ff
James Go <jamescookgo@gmail.com>
2015-04-21 07:29:38 -0700

    syntax bug fixes, didn't put some commas

--- js/game_control.js
+++ js/game_control.js
@@ -9,11 +9,11 @@
 
     // genotype items, custom designed for testing. use a method of random generation for picking from a preconfigured set for later
     this.subrules = {
-      "one_copy_dominance": new Subrule([0], [3], [0])
-      "codominance_both": new Subrule([0,1], [3,3], [0,0])	// first allele of first and second gene both have at least one copy
-      "codominance_first": new Subrule([0,1], [3,0], [0,0])	// first allele of first gene has one or two copies, first allele of second gene has zero copies
-      "codominance_second": new Subrule([0,1], [0,3], [0,0])	// first allele of first gene has zero copies, first allele of second gene has one or two copies
-      "two_copy_dominance": new Subrule([0], [2], [0])
+      "one_copy_dominance": new Subrule([0], [3], [0]),
+      "codominance_both": new Subrule([0,1], [3,3], [0,0]),	// first allele of first and second gene both have at least one copy
+      "codominance_first": new Subrule([0,1], [3,0], [0,0]),	// first allele of first gene has one or two copies, first allele of second gene has zero copies
+      "codominance_second": new Subrule([0,1], [0,3], [0,0]),	// first allele of first gene has zero copies, first allele of second gene has one or two copies
+      "two_copy_dominance": new Subrule([0], [2], [0]),
       "incomplete_dominance": new Subrule([0,0], [1,1], [0,1])	// both first and second allele of first gene each have one copy
     };
 
@@ -21,9 +21,9 @@
       "simple": new Rule([this.subrules["one_copy_dominance"]]),
       "codominance_both": new Rule(this.subrules["codominance_both"]),
       "codominance_first": new Rule(this.subrules["codominance_first"]),
-      "codominance_second": new Rule(this.subrules["codominance_second"])
-      "epistasis_first": new Rule(this.subrules["one_copy_dominance"])	// if the first gene has the first allele, all other genes don't matter
-      "epistasis_second": new Rule(this.subrules["codominance_second"])
+      "codominance_second": new Rule(this.subrules["codominance_second"]),
+      "epistasis_first": new Rule(this.subrules["one_copy_dominance"]),	// if the first gene has the first allele, all other genes don't matter
+      "epistasis_second": new Rule(this.subrules["codominance_second"]),
       "two_copy_dominance": new Rule(this.subrules["two_copy_dominance"]),
       "incomplete_dominance": new Rule(this.subrules["incomplete_dominance"])
     };
@@ -43,7 +43,7 @@
       // spotted: co dominance - B-cc = striped, bbC- = spotted, B-C- = striped and spotted, bbcc = plain
       new Phenotype(
         [1,2],	// uses second and third genes
-        ["striped and spotted", "striped", "spotted", "plain"]
+        ["striped and spotted", "striped", "spotted", "plain"],
         [this.rules["codominance_both"], this.rules["codominance_first"], this.rules["codominance_second"]]
       ),
 
