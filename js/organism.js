1755ff514d73f16a801c7a3f9b18d9d83ba599e4
James Go <jamescookgo@gmail.com>
2015-04-23 08:08:12 -0700

    moved getexpression to phenotype prototype

--- js/organism.js
+++ js/organism.js
@@ -6,21 +6,6 @@ function Organism(genome){
   var global_phenotypes = window["game_objects"].phenotypes;
   for(var i = 0; i < global_phenotypes.length; i++){
     var this_phenotype = global_phenotypes[i];
-    this.expressions.push(this.getExpression(this_phenotype, this.genome));
+    this.expressions.push(this_phenotype.getExpression(this.genome));
   }
 }
-
-Organism.prototype.getExpression = function(phenotype, genome){
-  var pheno_genes = phenotype.genes;
-  var i = 0;
-  for(; i < phenotype.rules.length; i++){
-    var rule = phenotype.rules[i];
-    for(var j = 0; j < rule.subrule.length; j++){
-      var subrule = rule.subrule[j];
-      if(subrule.subruleTrue(pheno_genes, genome)){
-        return phenotype.phenotypes[i];	// since "or" delimited, immediately return true
-      }
-    }
-  }
-  return phenotype.phenotypes[i];	// default last to last phenotype
-};
--- js/phenotype.js
+++ js/phenotype.js
@@ -1,5 +1,26 @@
 function Phenotype(genes, phenotypes, rules){
+  if(genes.constructor != Array || pehenotypes.constructor != Array || rules.constructor != Array) throw new Error("phenotype arguments much be of type array");
+  if(rules.length != phenotypes.length - 1) throw new Error("length of rules must be one less than length of phenotypes");
+
   this.genes = genes;	// array of gene indexes corresponding to the index in Organism.genome
   this.phenotypes = phenotypes;	// array of possible phenotype expressions
   this.rules = rules;	// array of Rule, rules.length == phenotypes.length - 1
 }
+
+Phenotype.prototype.getExpression = function(genome){
+  var pheno_genes = this.genes;
+  var rules = this.rules;
+  var phenotypes = this.phenotypes;
+
+  var i = 0;
+  for(; i < rules.length; i++){
+    var rule = rules[i];
+    for(var j = 0; j < rule.subrule.length; j++){
+      var subrule = rule.subrule[j];
+      if(subrule.subruleTrue(pheno_genes, genome)){
+        return phenotypes[i];	// since "or" delimited, immediately return true
+      }
+    }
+  }
+  return phenotypes[i];	// default last to last phenotype
+};
