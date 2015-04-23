b2dd20a73d568f2fec5e641064b4c9fc6baf493d
James Go <jamescookgo@gmail.com>
2015-04-22 17:58:55 -0700

    added method to check if array in constructors for rules and subrules

--- js/pt.js
+++ js/pt.js
@@ -19,6 +19,7 @@ function Phenotype(genes, phenotypes, rules){
 }
 
 function Subrule(gene_index, ncopies, allele_index){
+  if(gene_index.constructor != Array || ncopies.constructor != Array || allele_index.constructor != Array) throw new Error("subsubrules must be array");
   if(gene_index.length != ncopies.length || gene_index.length != allele_index.length) throw new Error("sub rule lengths must match");
   if(ncopies > 3 || ncopies < 0) throw new Error("number of copies in subrule must be >= 0 and <= 3");
   this.gene_index = gene_index;	// sub-subrules are "and" delimited - all must be true for the subrule to be true
@@ -27,6 +28,7 @@ function Subrule(gene_index, ncopies, allele_index){
 }
 
 function Rule(subrule){
+  if(subrule.constructor != Array) throw new Error("subrules must be an array");
   this.subrule = subrule;	// subrules are "or" delimited - just one subrule must be true for the rule to be true
 }
 
