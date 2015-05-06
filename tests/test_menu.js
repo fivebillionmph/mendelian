a1ab26c4bf7195fc8f02bb42be1561ab1f4fa51e
James Go <jamescookgo@gmail.com>
2015-05-06 08:03:39 -0700

    added more tests for menu

--- tests/test_menu.js
+++ tests/test_menu.js
@@ -4,15 +4,17 @@ QUnit.test("menu", function(assert){
   var sp3 = new SeedPouch(org1, org2, randomInt, random1)
 
   var menu = window.game_objects.menu;
-  assert.equal(menu.seed_pouches.length, 0, "make sure there are no seed pouches");
+  assert.equal(menu.seed_pouches.length, 0, "there are no seed pouches");
   menu.addSeedPouch(sp1);
   menu.addSeedPouch(sp2);
   menu.addSeedPouch(sp3);
-  assert.equal(menu.seed_pouches.length, 3, "make sure there are now three seed pouches");
+  assert.equal(menu.seed_pouches.length, 3, "there are now three seed pouches");
   menu.addSeedPouch(sp3);
-  assert.equal(menu.seed_pouches.length, 3, "make sure the same seed pouch was not added twice");
+  assert.equal(menu.seed_pouches.length, 3, "same seed pouch was not added twice");
   menu.removeSeedPouch(sp2);
-  assert.equal(menu.seed_pouches.length, 2, "make sure seed pouch was removed");
-  assert.equal(menu.seed_pouches[0], sp1, "make sure the first seed pouch is still in");
-  assert.equal(menu.seed_pouches[1], sp3, "make sure the third seed pouch is still in");
+  assert.equal(menu.seed_pouches.length, 2, "seed pouch was removed");
+  assert.equal(menu.seed_pouches[0], sp1, "first seed pouch is still in");
+  assert.equal(menu.seed_pouches[1], sp3, "third seed pouch is still in");
+  assert.notEqual(menu.seed_pouches[0], sp2, "second seed pouch not in first slice");
+  assert.notEqual(menu.seed_pouches[1], sp2, "second seed pouch not in second slice");
 });
