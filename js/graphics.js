c51c275c702f455e1da64b39ce5033d1a1205084
James Go <jamescookgo@gmail.com>
2015-05-11 20:57:22 -0700

    adjusted index for zebra graphics

--- js/graphics.js
+++ js/graphics.js
@@ -8,9 +8,6 @@ zebra.ready(function(){
   var bgcolor = "#1E90FF";	// dodger blue :)
   var pancol = "green";
 
-  // import namespaces
-  eval(zebra.Import("layout", "ui"));
-
   // initialize canvas and background panel
   var root = zebraRoot(sizex, sizey);
   var back_panel = zebraBackPanel(sizex, sizey, bgcolor);
@@ -19,14 +16,14 @@ zebra.ready(function(){
   zebraPanels(sizex, sizey, border_size, pancol);
 });
 
-function zebraRoot(sizey, sizex){
-  var root = (new zCanvas(sizex, sizey)).root;
+function zebraRoot(sizex, sizey){
+  var root = (new zebra.ui.zCanvas(sizex, sizey)).root;
   window.game_objects.zebra.root = root;
   return root;
 }
 
 function zebraBackPanel(sizex, sizey, bgcolor){
-  var back_panel = new Panel();
+  var back_panel = new zebra.ui.Panel();
   back_panel.setBounds(0,0,sizex,sizey);
   back_panel.setBackground(bgcolor);
   window.game_objects.zebra.root.add(back_panel);
@@ -35,21 +32,21 @@ function zebraBackPanel(sizex, sizey, bgcolor){
 }
 
 function zebraPanels(sizex, sizey, border_size, pancol){
-  var left_panel = new Panel();
+  var left_panel = new zebra.ui.Panel();
   left_panel.setBounds(border_size, border_size, (sizex/4) - (border_size * 2), sizey - (border_size * 2));
   left_panel.setBackground(pancol);
 
-  var right_panel = new Panel();
+  var right_panel = new zebra.ui.Panel();
   right_panel.setBounds(sizex - (sizex/4) + border_size, border_size, (sizex/4) - border_size * 2, sizey - (border_size * 2));
   right_panel.setBackground(pancol);
 
-  var center_panel = new Panel();
+  var center_panel = new zebra.ui.Panel();
   center_panel.setBounds(0,0);
   center_panel.setPreferredSize((sizex/2) - (border_size * 2), sizey - (border_size * 2));
   center_panel.setBackground(pancol);
 
-  var scroll_center_panel = new ScrollPan(center_panel, VERTICAL);
-  scroll_center_panel.setBounds((sizex/4) + (border_size * 2), border_size, (sizex/2) - (border_size * 2), sizey - (border_size * 2));
+  var scroll_center_panel = new zebra.ui.ScrollPan(center_panel, zebra.layout.VERTICAL);
+  scroll_center_panel.setBounds((sizex/4) + border_size, border_size, (sizex/2) - (border_size * 2), sizey - (border_size * 2));
 
   var go_zebra = window.game_objects.zebra;
   var back_panel = go_zebra.back_panel;
