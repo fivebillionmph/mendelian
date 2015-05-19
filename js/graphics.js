75f20ba9eb6e651fadc1e1713b511627a0ccc926
James Go <jamescookgo@gmail.com>
2015-05-19 08:00:13 -0700

    started graphics classes

---
+++ js/graphics.js
@@ -0,0 +1,14 @@
+function MenuPanel(menu_objects, menu_column, states){
+  this.menu_objects = menu_objects;
+  this.menu_column = menu_column;
+  this.states = states;
+  this.current_state = states[0];
+}
+
+MenuPanel.prototype.update = function(){
+  this.current_state.update(this.menu_objects, this.menu_column, this);
+}
+
+MenuPanel.prototype.changeState = function(idx){
+  this.current_states = states[idx];
+}
