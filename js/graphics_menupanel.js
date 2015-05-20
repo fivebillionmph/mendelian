d6bec757ed0d1ab93610ccbae4798fd7a72bc91f
James Go <jamescookgo@gmail.com>
2015-05-19 18:01:14 -0700

    added more to menu state left panel

--- js/graphics_menupanel.js
+++ js/graphics_menupanel.js
@@ -2,11 +2,12 @@ function MenuPanel(menu_objects, menu_column, states, properties){
   this.menu_objects = menu_objects;
   this.menu_column = menu_column;
   this.states = states;
+  this.properties = properties;
   this.current_state = states[0];
 }
 
 MenuPanel.prototype.update = function(){
-  this.current_state.update(this.menu_objects, this.menu_column, this);
+  this.current_state.update(this.menu_objects, this.menu_column, this.properties, this);
 }
 
 MenuPanel.prototype.changeState = function(idx){
--- js/menustates.js
+++ js/menustates.js
@@ -1,14 +1,14 @@
 $(function(){
+  state0 = function(menu_objects, menu_column, properties, parent_panel){
+    
+  }
+
+
+
+
+  var menu_objects = window.game_objects.menu_objects;
   var properties = window.game_objects.properties;
-
-
-
-
-
-
-
-
-
+  var left_panel = $("left-panel");
   var left_panel_states = [
     new MenuState(state0),
     new MenuState(state1)
