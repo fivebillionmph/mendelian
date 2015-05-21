219ae9924d5db15bc708adbe87bbb1b0ae0b5e17
James Go <jamescookgo@gmail.com>
2015-05-20 21:46:15 -0700

    added more menustates functions

--- js/menustates.js
+++ js/menustates.js
@@ -8,7 +8,7 @@ $(function(){
  
     // draw seed pouches
     for(var i = 0; i < seed_pouches.length; i++){
-      addSeedPouch(seed_pouches[i], i, sp_props, menu_column);
+      addSeedPouch(seed_pouches[i], i, sp_props, menu_column, parent_menu);
     }
   };
 
@@ -34,12 +34,22 @@ $(function(){
     return bounds;
   };
 
-  var addSeedPouch = function(seed_pouch, idx, sp_props, menu_column){
+  var addSeedPouch = function(seed_pouch, idx, sp_props, menu_column, parent_menu){
     var column = (idx + 1) % sp_props.n_per_row;
     var row = Math.floor(idx / sp_props.n_per_row);
-    var top_offset = (row - 1) * sp_props.full_size_height;
-    var left_offset = (column - 1) * sp_props.full_size_width;
-    
+    var top_offset = (row - 1) * sp_props.full_size_height + sp_props.border;
+    var left_offset = (column - 1) * sp_props.full_size_width + sp_props.border;
+    var on_click_fun = onClickFun(seed_pouch, parent_menu);	// need to implement
+    $("<div/>", {
+      css: {
+        position: "absolute",
+        top: top_offset,
+        left: left_offset,
+        width: sp_props.width,
+        height: sp_props.height
+      },
+      onClick: on_click_fun,
+    }
   };
 
   var left_panel_states = [
