9c0bb6f761db0eeb4eaa79e2c23397d208c8d78b
James Go <jamescookgo@gmail.com>
2015-05-21 07:58:02 -0700

    updated menustates and properties in game control

--- js/game_control.js
+++ js/game_control.js
@@ -10,9 +10,22 @@ function random1(){
   window.game_objects = window.game_objects || {};
 
   window.game_objects.properties = {
-
+    seed_pouch_width: 50,
+    seed_pouch_height: 50,
+    seed_pouch_image: "images/seed_pouch_image.png",
+    seed_pouch_border: 5
   };
 
 
   window.game_objects.menu_objects = new Menu();
+
+
+  window.game_objects.menu_states = {
+    seed_pouches_all: 0,
+    seed_pouches_single: 1,
+    plots_all: 0,
+    plots_single: 1,
+    slots_single: 2,
+    seed_pouches_builder: 0
+  };
 }());
